ba35043de0f2e45bdf247870724ee83b0f1d7244
TumharaGitHubName <tumhara@email.com>
2026-02-22 18:09:02 +0500

    updated

---
+++ practice_assign/app.js
@@ -0,0 +1,12 @@
+// function Students(name, color){
+//     this.name = name,
+//     this.color = color
+// }
+
+// Students.prototype.greet = function(){
+//     console.log(`my name is ${this.name} my fourite color is ${this.color}`);
+// }
+
+// let card = new Students("hassaan", "red")
+// card.greet()
+
