7ddeacbe22a178d9faee817232f2bda5d6554788
TumharaGitHubName <tumhara@email.com>
2026-02-24 16:41:50 +0500

    add initial HTML and JavaScript files for practice class works

---
+++ practice_class_works/app.js
@@ -0,0 +1,37 @@
+// var data = {
+//     student: {
+//         name: "hassaan",
+//         roll: 123,
+//         class: 11,
+//     }
+// }
+
+// for (let i in data) {
+//     for (let j in data[i])
+//     console.log(data[i][j]);
+// }
+
+
+// console.log(Object.keys(data));
+// console.log(Object.values(data));
+// console.log(Object.entries(data));
+
+
+
+// var pageOpen;
+// function openWindow(){
+//     pageOpen = window.open("home.html")
+// }
+
+// function closeWindow(){
+//     if(pageOpen){
+//         pageOpen.close();
+//     }
+// }
+
+
+
+// /^[0-9]{5}-[0-9]{7}-[0-9]$/.test("12345-1234567-2")
+// true
+// /^[0-9]{5}-[0-9]{7}-[0-9]$/.test("10000-1284247-2")
+// true
