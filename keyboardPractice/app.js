695b5bb649c0bb1d4163e86eeb4b247b27e6371e
TumharaGitHubName <hassaanali994@gmail.com>
2026-04-06 16:52:16 +0500

    Implement keyboard input handling and add result button in HTML

--- keyboardPractice/app.js
+++ keyboardPractice/app.js
@@ -1,7 +1,17 @@
 var get = document.getElementById("name")
 
-document.addEventListener("keydown", function(e){
+document.addEventListener("keydown", function (e) {
 
-    if(e.key == "w")
-        get.value = get.value + e.key
+    if (e.key === "Enter") {
+        show();
+    } else if (e.key === "Backspace") {
+        get.value = get.value.slice(0, -1)
+    } else if (e.key.length == 1) {
+        get.value += e.key;
+    }
 })
+
+function show() {
+    console.log(get.value);
+    get.value = ""
+}
