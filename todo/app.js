47fb19baa4099d86e78250a46edecd2b83e3068d
TumharaGitHubName <hassaanali994@gmail.com>
2026-05-08 19:34:07 +0500

    updated

--- todo/app.js
+++ todo/app.js
@@ -2,41 +2,54 @@ let inp = document.getElementById("input");
 let btn = document.getElementById("result");
 let con = document.getElementById("container");
 
+// let myTodo = JSON.parse(localStorage.getItem("mytodo")) || []
+// myTodo.push("hassaan");
+// localStorage.setItem("myTodo", JSON.stringify(myTodo));
+// console.log(myTodo);
 
-btn.addEventListener("click",()=>{
-    let main = document.createElement("div");
-    let para = document.createElement("p");
-    let btn1 = document.createElement("button");
-    let btn2 = document.createElement("button");
-    btn1.id = "delBtn"
-    btn2.id = "ediBtn"
+// let saveItem = () => {
+// }
 
-    para.innerText = inp.value;
-    console.log(para);
+// let renderTodo = () => {
+//     con.innerText = "";
 
-    btn1.innerText = "Delete";
-    btn2.innerText = "Edit";
+//     myTodos.forEach((todo, index) => {
+//         console.log(todo);
+        
+// //         let main = document.createElement("div");
+// //         let para = document.createElement("p");
+// //         let btn1 = document.createElement("button");
+// //         let btn2 = document.createElement("button");
 
-    para.appendChild(btn1);
-    para.appendChild(btn2);
-    main.appendChild(para);
-    con.appendChild(main);
+// //         para.innerText = todo;
+// //         console.log(para);
 
-    inp.value = "";
+// //         btn1.innerText = "Delete";
+// //         btn2.innerText = "Edit";
 
-    delBtn.addEventListener("click", ()=>{
-        event.target.parentNode.parentNode.remove()
-    })
+// //         para.appendChild(btn1);
+// //         para.appendChild(btn2);
+// //         main.appendChild(para);
+// //         con.appendChild(main);
 
-    ediBtn.addEventListener("click", (e)=>{
-        let p = e.target.parentNode;
-        let oldTe = p.firstChild.nodeValue;
+// //         inp.value = "";
 
-        let newText = prompt("Enter your new Value", oldTe);
+// //         delBtn.addEventListener("click", () => {
+// //             event.target.parentNode.parentNode.remove()
+// //         })
 
-        if (newText !== "" && newText !== null) {
-            p.firstChild.nodeValue = newText;
-        }
-    })
-})
+// //         ediBtn.addEventListener("click", (e) => {
+// //             let p = e.target.parentNode;
+// //             let oldTe = p.firstChild.nodeValue;
+
+// //             let newText = prompt("Enter your new Value", oldTe);
+            
+// //             if (newText !== "" && newText !== null) {
+// //                 p.firstChild.nodeValue = newText;
+// //             }
+// //         })
+    
+// });
+
+// };
 
