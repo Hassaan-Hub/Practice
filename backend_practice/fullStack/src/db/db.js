0ae1c2f178c97323bc729173cfc057b6a5ef1280
https://github.com/Hassaan-Hub <hassaanali994@gmail.com>
2026-09-25 12:34:36 +0500

    updated

--- backend_practice/fullStack/src/db/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const connectDB = async () => {
-    await mongoose.connect(process.env.MONGODB_URI)
-
-    console.log('Mongo DB connected');
-}
-
-module.exports = connectDB;
--- backend_practice/fullStack/src/register/register.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { register } = require("../controllers/signup/signup.post.controller")
-
-const register = express.Router();
-
-register.post('/register', register)
-
-
-module.exports = register;
